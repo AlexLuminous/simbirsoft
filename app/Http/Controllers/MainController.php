34feeb92f5bf75acf488071ff0b3835b8401bbd4
Alex <letit.depesha@mail.ru>
2017-11-28 20:11:39 +0400

    Fix whitespaces

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -15,38 +15,38 @@ class MainController extends Controller {
      */
     public function showFibonacci(Request $request)
     {
-    	if ($request->input('count') !== null) {
-    		$count = (int) $request->input('count');
-    	} else {
-    		$count = 20;
-    	}
-		$arNums = $this->fibonacci($count);
+        if ($request->input('count') !== null) {
+            $count = (int) $request->input('count');
+        } else {
+            $count = 20;
+        }
+        $arNums = $this->fibonacci($count);
         return view('fibonacci', ['arNums' => $arNums]);
     }
 
     protected function fibonacci($n)
     {
-    	if ($n === 1) {
-    		return ['0'];
-    	} elseif ($n === 2) {
-    		return ['0, ', '1'];
-    	} else {
-	    	$n = $n - 3;
-			$a = 0;
-			$b = 1;
-			$r = 0;
-			$numbers = ["0, ", "1, "];
-			for ($i = 0; $i <= $n; $i++) {
-			    $r = $a + $b;
-			    $a = $b;
-			    $b = $r;
-			    if ($i == $n) {
-			        $numbers[] = $r;
-			    } else {
-			        $numbers[] = $r.', ';
-			    }
-			}
-			return $numbers;
-		}
+        if ($n === 1) {
+            return ['0'];
+        } elseif ($n === 2) {
+            return ['0, ', '1'];
+        } else {
+            $n = $n - 3;
+            $a = 0;
+            $b = 1;
+            $r = 0;
+            $numbers = ["0, ", "1, "];
+            for ($i = 0; $i <= $n; $i++) {
+                $r = $a + $b;
+                $a = $b;
+                $b = $r;
+                if ($i == $n) {
+                    $numbers[] = $r;
+                } else {
+                    $numbers[] = $r.', ';
+                }
+            }
+            return $numbers;
+        }
     }
 }
